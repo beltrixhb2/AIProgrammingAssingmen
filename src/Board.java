3748738e59f615b9c065e89af1c3bbdfca0554f8
ggronda <jesusggironda01@gmail.com>
2022-10-31 21:46:01 +0200

    comentario

--- src/Board.java
+++ src/Board.java
@@ -2,6 +2,9 @@ public class Board {
 
     private final char[][] board = new char[7][6];
 
+    /**
+     * This method is the constructor that initialise a empty board
+     */
     public Board() {
         for (int i = 0; i < 7; i++) {
             for (int j = 0; j < 6; j++) {
@@ -10,6 +13,10 @@ public class Board {
         }
     }
 
+    /**
+     * This method copy the board
+     * @param copy
+     */
     public Board(Board copy) {
         for (int i = 0; i < 7; i++) {
             for (int j = 0; j < 6; j++) {
@@ -18,11 +25,20 @@ public class Board {
         }
     }
 
-
+    /**
+     * This method return a position in the board.
+     * The parameter are the coordinates
+     * @param x
+     * @param y
+     * @return
+     */
     public char getxy(int x, int y) {
         return board[x][y];
     }
 
+    /**
+     * This method prints the board
+     */
     public void printBoard() {
         for (int i = 0; i < 7; i++) {
             for (int j = 0; j < 6; j++) {
@@ -32,7 +48,10 @@ public class Board {
         }
     }
 
-
+    /**
+     * This method check if the board is full, that implies tie
+     * @return
+     */
     public boolean tie() {
         boolean full = true;
         for (int i = 0; i < 6; i++) {
@@ -43,6 +62,12 @@ public class Board {
         return full;
     }
 
+    /**
+     * This method checks if there are 4 pieces in a row.
+     * This method uses tie method
+     * @return [-1,0,1]
+     * IA lost, tie, PLayer win
+     */
     public int checkfinish() {
         boolean finish = false;
         int count;
@@ -98,9 +123,16 @@ public class Board {
                 }
             }
         }
+        //For checking the diagonals we
+        //separate de diagonals in two types: growing and decreasing diagonals
+        //and in each one we do two diferents loops to evaluate the diagonals above and below the main diagonals
+
+
+
         //Check growing diagonals
-        for (int i = 5; i >= 0; i--) {//ciclo descendente
-            int col = i, row = 0;  //declaramos los valores que seran los indices a imprimir
+        //above the main diagonal
+        for (int i = 5; i >= 0; i--) {
+            int col = i, row = 0;
             count = 0;
             anterior = '*';
             while (col < 6 && row < 7) {
@@ -123,9 +155,10 @@ public class Board {
                 row++;
 
             }
-            for (int j = 0; j < 6; j++) {//ciclo descendente
+            // below the main diagonal
+            for (int j = 0; j < 6; j++) {
                 row = 1 + j;
-                col = 0;  //declaramos los valores que seran los indices a imprimir
+                col = 0;  //
                 count = 0;
                 anterior = '*';
                 while (col < 6 && row < 7) {
@@ -153,8 +186,9 @@ public class Board {
             }
         }
         //Check decreasing diagonals
-        for (int i = 6; i >= 0; i--) {//ciclo descendente
-            int col = 0, row = i;  //declaramos los valores que seran los indices a imprimir
+        //above the main diagonal
+        for (int i = 6; i >= 0; i--) {
+            int col = 0, row = i;
             count = 0;
             anterior = '*';
             while (col < 6 && row >= 0) {
@@ -178,10 +212,10 @@ public class Board {
 
             }
         }
-
+        //below the main diagonal
         for (int j = 0; j < 6; j++) {
             int row = 6;
-            int col = j;  //declaramos los valores que seran los indices a imprimir
+            int col = j;
             count = 0;
             anterior = '*';
             while (col < 6 && row >= 0) {
@@ -218,6 +252,13 @@ public class Board {
         }
     }
 
+    /**
+     * This method insert a piece in a column. The palyer should be declared because
+     *each player has in own pieces
+     * @param column
+     * @param player
+     * @return
+     */
     public boolean insert(int column, int player) {
         boolean correct = true;
         if (board[0][column] == '*') {
@@ -236,6 +277,10 @@ public class Board {
         return correct;
     }
 
+    /**
+     * This method works as a signal method using the checkfinish.
+     * @return
+     */
     public boolean finish() {
         if (checkfinish() == 0) {
             return false;
@@ -243,19 +288,28 @@ public class Board {
         return true;
     }
 
+    /**
+     * This method checks if a column is empty.
+     * @param column
+     * @return
+     */
     public boolean columnfree(int column) {
         return (board[0][column] == '*');
     }
 
-
+    /**
+     * This method
+     *
+     * @return
+     */
     public int evaluate() {
         boolean finish;
         int score = 0;
-        int count;
-        int countmax;
-        int countmin;
-        int countxmax;
-        int countomin;
+        int count;//count same pieces in a row
+        int countmax;//count avaible positions for IA
+        int countmin;// count avaivble positions for playeer
+        int countxmax;// IA x in the posible 4
+        int countomin;// player 0 in the posible 4
         char anterior;
         //Check rows
         for (int i = 0; i < 7; i++) {
@@ -322,7 +376,7 @@ public class Board {
             countxmax = 0;
             countomin = 0;
             countmax = 0;
-            for (int i = 0; i < 7; i++) {
+            for (int i = 6; i >= 0; i--) {
                 if (board[i][j] == '*'||board[i][j] == 'O'){
                     countmin++;
                     if (board[i][j] == 'O'){
